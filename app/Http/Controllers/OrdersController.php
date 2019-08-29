381a49ec5226b342887438b76132c63d85c105bc
lennan747 <lennan747@gmail.com>
2019-08-29 10:02:15 +0800

    测试一下切换分支

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,6 +12,7 @@ class OrdersController extends Controller
     //
     public function index(OrderRequest $request)
     {
+        dd(1111);
         $query = $request->user()->orders()->withOrder($request->order);
 
         $where = [];
