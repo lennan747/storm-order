69b5fd01c7bbd06b8e0c339e1c338d4930002def
lennan747 <lennan747@gmail.com>
2019-08-30 15:37:03 +0800

    显示登录模板

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,7 +44,7 @@ class LoginController extends Controller
 
     public function showLoginForm()
     {
-        //return view('auth.login');
-        return view('myauth.login');
+        return view('auth.login');
+        //return view('myauth.login');
     }
 }
