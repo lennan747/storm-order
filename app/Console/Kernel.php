f0b462a920327ab537948d9f1ef44934d6255a80
lennan747 <lennan747@gmail.com>
2019-09-18 14:16:40 +0800

    定时任务时间，每天一次

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('larabbs:calculate-active-user')->hourly()->daily();
+        $schedule->command('larabbs:calculate-active-user')->daily();
     }
 
     /**
